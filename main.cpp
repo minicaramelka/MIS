ca1a37eef6b5e2f9fb263e16803affffa030718d
Lokol1337 <58722409+Lokol1337@users.noreply.github.com>
2020-04-05 14:20:35 +0300

    Update main.cpp
    Круто не работает на 16-28(перегрузка оператора [])

--- main.cpp
+++ main.cpp
@@ -9,8 +9,33 @@ LList::LList()
     {
         head = NULL;
     }
+LList::~LList()
+    {
 
-void push_back(int val, Node *head)
+    }
+int& LList::operator[](const size_t pos) const
+{
+	return getNode(pos)->value;
+}
+
+LList::Node* LList::getNode(const size_t pos) const
+{
+	if (pos < 0) {
+		assert(pos < 0);
+	}
+	else if (pos >= this->_size) {
+		assert(pos >= this->_size);
+	}
+
+	Node* bufNode = this->_head;
+	for (int i = 0; i < pos; ++i) {
+		bufNode = bufNode->next;
+	}
+
+	return bufNode;
+}
+
+void LList::push_back(int val)
     {
         Node *nd = new Node;
         nd->data = val;
@@ -26,7 +51,7 @@ void push_back(int val, Node *head)
         }
     }
 
-void push_front(int val,Node *head)
+void LList::push_front(int val)
     {
     Node *nd = new Node[val];
 
@@ -43,7 +68,7 @@ void push_front(int val,Node *head)
         }
     }
 
-void pop_back(int val, Node *head)
+void LList::pop_back(int val)
     {
     Node *current = head;
 
@@ -51,7 +76,7 @@ void pop_back(int val, Node *head)
 
     }
 
-void pop_front(int val, Node *head)
+void LList::pop_front(int val)
     {
     if(head)
         {
@@ -66,20 +91,27 @@ void pop_front(int val, Node *head)
 
 //size_t size() const;
 
-void erase_at(size_t idx)
+void LList::erase_at(size_t idx)
     {
 
     }
-void insert_at(size_t idx, int val)
+void LList::insert_at(size_t idx, int val)
     {
 
     }
-void reverse()
+void LList::reverse()
     {
 
     }
 
 int main()
     {
+    LList l;
+    l.push_back(1);
+	l.push_front(0);
+	l.push_back(2);
+	for(size_t i = 0; i < 3; ++i, cout << " ")
+        cout << l[i];
+
 
     }
