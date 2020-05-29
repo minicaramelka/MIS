6a1ddb6d5cb054329aa9d5dd0ee03b22b05cb869
minicaramelka <59147993+minicaramelka@users.noreply.github.com>
2020-05-29 16:25:48 +0300

    Исправлен диструктор и переведено на шаблоны

--- llist.h
+++ llist.h
@@ -4,28 +4,29 @@
 
 // Linked list of integers
 
+template<typename T>
 class LList{
     struct Node{
-        int data;
+        T data;
         Node *next;
     };
 
 	private:
-    Node *head;
-	public:
-	LList();                         // construct new collection
-	~LList();                        // free resources
+		Node *head;
 
-	void push_back(int val);         // add new value at the end:  [1 2 3 4] -> [1 2 3 4 5]
-	void push_front(int val);        // add new value at the begin [1 2 3 4] -> [5 1 2 3 4]
-	void pop_back();          // remove at the end          [1 2 3 4] -> [1 2 3]
-	void pop_front();         // remove at the front        [1 2 3 4] -> [2 3 4]
-	size_t size() const;             // get actual number of items [1 2 3 4] -> 4
-	int& operator[](size_t idx);     // get rw access ot specific item addressing by idx
-	int  operator[](size_t idx)const; //get read-only access
-	void erase_at(size_t idx);       // remove item at specific position: [1 2 3 4], 2 -> [1 2 4]
-	void insert_at(size_t idx, int val); // insert item at specific position: [1 2 3 4], 1, 5 -> [1 5 2 3 4]
-	void reverse();    // reverse item sequense: [1 2 3 4] -> [4 3 2 1]
+	public:
+		LList();                         // construct new collection
+		~LList();                        // free resources
+		void push_back(T val);         // add new value at the end:  [1 2 3 4] -> [1 2 3 4 5]
+		void push_front(T val);        // add new value at the begin [1 2 3 4] -> [5 1 2 3 4]
+		void pop_back();          // remove at the end          [1 2 3 4] -> [1 2 3]
+		void pop_front();         // remove at the front        [1 2 3 4] -> [2 3 4]
+		size_t size() const;             // get actual number of items [1 2 3 4] -> 4
+		T& operator[](size_t idx);     // get rw access ot specific item addressing by idx
+		T operator[](size_t idx)const; //get read-only access
+		void erase_at(size_t idx);       // remove item at specific position: [1 2 3 4], 2 -> [1 2 4]
+		void insert_at(size_t idx, T val); // insert item at specific position: [1 2 3 4], 1, 5 -> [1 5 2 3 4]
+		void reverse();    // reverse item sequense: [1 2 3 4] -> [4 3 2 1]
 };
 
 #endif //LLIST_H
